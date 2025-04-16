9de9aa679fb5a2fe8767c3788366e6a8278fcb75
Ranjan Bhatnagar <67755323+RanjanBhatnagar@users.noreply.github.com>
2025-04-16 12:25:36 +0530

    StudentCrud in Angular
    Ref Material

---
+++ Angular/StudentsAPI/StudentsAPI/Models/StudentContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace StudentsAPI.Models;
+
+public partial class StudentContext : DbContext
+{
+    public StudentContext()
+    {
+    }
+
+    public StudentContext(DbContextOptions<StudentContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<TblStudent> TblStudents { get; set; }
+
+    
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TblStudent>(entity =>
+        {
+            entity.HasKey(e => e.StudentId).HasName("PK__tbl_Stud__32C52B99F87ECF3C");
+
+            entity.ToTable("tbl_Student");
+
+            entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+            entity.Property(e => e.ModifiedDate).HasColumnType("datetime");
+            entity.Property(e => e.StudentGrade)
+                .HasMaxLength(50)
+                .HasColumnName("StudentGrade ");
+            entity.Property(e => e.StudentName).HasMaxLength(50);
+            entity.Property(e => e.StudentRollNo).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
